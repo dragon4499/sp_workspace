cc70d8e2f858372a59306a27628c310f3e377bb4
SooYong Chung <dragon4499@gmail.com>
2022-05-22 20:53:08 +0900

    SUB1 Completed

--- sp_java_test/SUB1/src/com/lgcns/test/RunManager.java
+++ sp_java_test/SUB1/src/com/lgcns/test/RunManager.java
@@ -1,10 +1,32 @@
 package com.lgcns.test;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class RunManager {
-
 	public static void main(String[] args) {
-		System.out.println("Hello, world");
+		Queue<String> messageQueue = new LinkedList<>();
 
+		Scanner sc = new Scanner(System.in);
+
+		while(sc.hasNextLine()) {
+			String input = sc.nextLine();
+			String[] command = input.split(" ");
+
+			switch(command[0]) {
+				case "SEND":
+					messageQueue.offer(command[1]);
+					break;
+				case "RECEIVE":
+					String msg = messageQueue.poll();
+					System.out.println(msg);				
+					break;
+				default:
+					break;
+			}
+		}
+
+		sc.close();
 	}
-
 }
